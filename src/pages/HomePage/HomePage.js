bb64f698f7817d297bc80785e390ec87854d2006
Rumies <rumies.media@gmail.com>
2021-01-03 21:47:26 -0500

    added temporary introductory content for Landing Page

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -13,6 +13,16 @@ const HomePage = () => {
     return (
         <PageContainer>
             <Logo logo={logo} altLogo={altLogo} />
+            <div>
+                <h3>Hi there. My name is</h3>
+                <h1>
+                    <span style={{ color: "orange"}}>R</span>uel <span style={{ color: "orange"}}>M</span>isa. <br />
+                    I build things.
+                </h1>
+
+                <p>I am a Front End Developer and Graphic Designer based in Brookyln, NY. I specialize in building creative User Interfaces.</p>
+                
+            </div>
             <Navbar />
         </PageContainer>
     );
