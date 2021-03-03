579175235324a9cc013927273f1005d6456c6234
Rumies <rumies.media@gmail.com>
2021-03-03 04:54:48 -0500

    finished hero component

--- src/components/Hero/Hero.elements.js
+++ src/components/Hero/Hero.elements.js
@@ -3,10 +3,16 @@ import { MainHeading, SubHeading } from '../../globalStyles';
 
 export const HeroContainer = styled.div`
     grid-column: full-start / full-end;
-
     display: flex;
     align-items: center;
-    border: 1px solid red;
+
+    /* background-color: var(--red-main);
+    color: var(--grey-light); */
+
+    @media screen and (max-width: 960px) {
+        flex-direction: column;
+        justify-content: center;
+    }
 `;
 
 export const IntroBox = styled.div`
@@ -16,12 +22,17 @@ export const IntroBox = styled.div`
     @media screen and (max-width: 960px) {
         margin-left: 10%;
         margin-right: 10%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        text-align: center;
     }
 `;
 
 export const IntroTitle = styled(MainHeading)`
     font-size: 4rem;
     letter-spacing: 0.4rem;
+
 `;
 
 export const IntroSubtitle = styled(SubHeading)`
